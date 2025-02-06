36f5c93a739be9d3cc570546f2d78da0d7c1d223
student <jeremypoliquit.student@gmail.com>
2025-02-05 17:30:31 -0800

    semantic structure nav/section/footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import RegisterForm from '../../components/RegisterForm'
-import UserTable from '../../components/UserTable'
-import ButtonIncrement from '../../components/ButtonIncrement'
-import StudentInfo from '../../components/Registration-Form/StudentInfo'
-import StudentChecklist from '../../components/Registration-Form/StudentChecklist'
+import React from "react";
+import RegisterForm from "../../components/RegisterForm";
+import UserTable from "../../components/UserTable";
+import ButtonIncrement from "../../components/ButtonIncrement";
+import StudentInfo from "../../components/Registration-Form/StudentInfo";
+import StudentChecklist from "../../components/Registration-Form/StudentChecklist";
+import Footer from "../../components/Registration-Form/Footer";
 
 const page = () => {
   return (
-    <div className='flex flex-col gap-8 min-h-screen'>
+    <div className="flex flex-col gap-8 min-h-screen mx-4">
       <nav>
         <StudentInfo />
       </nav>
 
-
       <section>
         <StudentChecklist />
       </section>
-    </div>
-  )
-}
 
-export default page
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default page;
