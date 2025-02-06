39c18ccb3e322e958f4f292ac3f24a0cbadfe848
student <jeremypoliquit.student@gmail.com>
2025-02-06 09:41:56 -0800

    modified components

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,18 +1,34 @@
-"use client"
-import React from 'react'
-import useForm from '../hooks/useForm'
+"use client";
+import React from "react";
+import useForm from "../hooks/useForm";
 
 const RegisterForm = () => {
   const { formData, handleChange, handleSubmit } = useForm({
-      initialize: { username: "", password: ""},
-  }); 
+    initialize: { username: "", password: "" },
+  });
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="text" value={formData.username} name='username' onChange={handleChange} className='input input-bordered w-full max-w-sm' placeholder='Username' />
-        <input type="password" value={formData.password} name='password' onChange={handleChange} className='input input-bordered w-full max-w-sm' placeholder='Password' />
-        <button type="submit" className='btn btn-success btn-wide text-white'>Enter</button>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <input
+        type="text"
+        value={formData.username}
+        name="username"
+        onChange={handleChange}
+        className="input input-bordered w-full max-w-sm"
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={formData.password}
+        name="password"
+        onChange={handleChange}
+        className="input input-bordered w-full max-w-sm"
+        placeholder="Password"
+      />
+      <button type="submit" className="btn btn-success btn-wide text-white">
+        Enter
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,15 @@ import React from "react";
 import RegisterForm from "../../components/RegisterForm";
 import UserTable from "../../components/UserTable";
 import ButtonIncrement from "../../components/ButtonIncrement";
-import StudentInfo from "../../components/Registration-Form/StudentInfo";
-import StudentChecklist from "../../components/Registration-Form/StudentChecklist";
-import Footer from "../../components/Registration-Form/Footer";
+import StudentInfo from "../../components/CertificateRegistration/StudentInfo";
+import StudentChecklist from "../../components/CertificateRegistration/StudentChecklist";
+import Footer from "../../components/CertificateRegistration/Footer";
+import RegisterPage from "./pages/registeraccount/page";
 
 const page = () => {
   return (
-    <div className="flex flex-col gap-8 min-h-screen mx-4">
-      <nav>
-        <StudentInfo />
-      </nav>
-
-      <section>
-        <StudentChecklist />
-      </section>
-
-      <footer>
-        <Footer />
-      </footer>
+    <div>
+      <h1>Go to pages url</h1>
     </div>
   );
 };
